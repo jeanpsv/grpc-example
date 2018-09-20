392349be82065b1cdc1b5de26fe8158bc7e00849
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-20 17:53:43 -0300

    remove warning from nodejs client

--- clients/nodejs/index.js
+++ clients/nodejs/index.js
@@ -1,15 +1,22 @@
 const grpc = require("grpc")
+const protoLoader = require("@grpc/proto-loader")
 
 const protoPath = require("path").join(__dirname, "../..", "protobufs")
 
-const proto = grpc.load({
-  root: protoPath,
-  file: "greeter.proto"
+const packageDefinition = protoLoader.loadSync(`${protoPath}/greeter.proto`, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
 })
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
 
-const client = new proto.greeter.GreeterService(`${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`, grpc.credentials.createInsecure())
+const GreeterService = new protoDescriptor.greeter.GreeterService(`${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`, grpc.credentials.createInsecure())
 
-client.hello("Jean Vasconcelos", (error, response) => {
+GreeterService.hello({
+	name: "Jean Vasconcelos"
+}, (error, response) => {
   if (error) {
     console.log(error)
     return
