e45d1180738a65c6b8182724ddc7bb146f28ad97
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-20 23:51:33 -0300

    improve python client with environment variables

--- clients/python/client.py
+++ clients/python/client.py
@@ -1,13 +1,15 @@
+import os
 import grpc
 import greeter_pb2
 import greeter_pb2_grpc
 
 def main():
-
-    with grpc.insecure_channel('server:50051') as channel:
+    host = os.environ.get('SERVER_HOST')
+    port = os.environ.get('SERVER_PORT')
+    with grpc.insecure_channel("{0}:{1}".format(host, port)) as channel:
         stub = greeter_pb2_grpc.GreeterServiceStub(channel)
         response = stub.Hello(greeter_pb2.Human(name='Jean Vasconcelos'))
-    print("Greeter client received: " + response.message)
+    print(response)
 
 if __name__ == '__main__':
     main()
