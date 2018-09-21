0bea2ae88538514f5b6fa8ee62f537205216dbcf
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-21 10:28:11 -0300

    implement gRPC ruby client

---
+++ clients/ruby/client.rb
@@ -0,0 +1,13 @@
+require 'grpc'
+require 'greeter_services_pb'
+
+def main
+  host = ENV["SERVER_HOST"]
+  port = ENV["SERVER_PORT"]
+  stub = Greeter::GreeterService::Stub.new("#{host}:#{port}", :this_channel_is_insecure)
+
+  response = stub.hello(Greeter::Human.new(name: "Jean Vasconcelos"))
+  p response
+end
+
+main()
